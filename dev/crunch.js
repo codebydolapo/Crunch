56d2da965637365dd6a31cf7567ac90b3c3e0bc1
codebydolapo <bashorun115@gmail.com>
2022-11-14 22:29:37 -0800

    initial deployment

---
+++ dev/crunch.js
@@ -0,0 +1,19 @@
+function Crunch() {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+Crunch.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+    const newBlock = {
+        //the block's number
+        index: this.chain.length + 1,
+        timeStamp: Date.now(),
+        transacions: this.newTransacions,
+        nonce,
+        hash,
+        previousBlockHash
+    }
+    this.newTransactions = [];
+    this.chain.push(newBlock)
+    return newBlock
+}
