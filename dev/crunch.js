de0c3561de4b97d3227df57497f3bf41dd8186d4
codebydolapo <bashorun115@gmail.com>
2022-11-15 07:02:34 -0800

    tweaked the crunch prototype

--- dev/crunch.js
+++ dev/crunch.js
@@ -1,21 +1,22 @@
 function Crunch() {
     this.chain = [];
     this.newTransactions = [];
-}
 
-Crunch.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
-    const newBlock = {
-        //the block's number
-        index: this.chain.length + 1,
-        timeStamp: Date.now(),
-        transacions: this.newTransacions,
-        nonce,
-        hash,
-        previousBlockHash
+
+    this.createNewBlock = (nonce, previousBlockHash, hash) => {
+        const newBlock = {
+            //the block's number
+            index: this.chain.length + 1,
+            timeStamp: Date.now(),
+            transacions: this.newTransacions,
+            nonce,
+            hash,
+            previousBlockHash
+        }
+        this.newTransactions = [];
+        this.chain.push(newBlock)
+        return newBlock
     }
-    this.newTransactions = [];
-    this.chain.push(newBlock)
-    return newBlock
-}
 
+}
 module.exports = Crunch
--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,10 @@
 const Crunch = require("./crunch");
 
 const crunch = new Crunch();
+async function mainTest(){
+    crunch.createNewBlock(72637, "82YRIUY274YR2", '726287463248H')
 
-console.log(crunch)
+    console.log(crunch)
+}
+
+mainTest()
