ca2c9a48dee810f4879e212abdfc1d9eb0703cb6
codebydolapo <bashorun115@gmail.com>
2022-12-27 09:41:29 -0800

    added createNewTransaction() method

--- dev/crunch.js
+++ dev/crunch.js
@@ -1,22 +1,44 @@
-function Crunch() {
-    this.chain = [];
-    this.newTransactions = [];
+class Crunch {
 
-
-    Crunch.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
-        const newBlock = {
-            //the block's number
-            index: this.chain.length + 1,
-            timeStamp: Date.now(),
-            transacions: this.newTransacions,
-            nonce,
-            hash,
-            previousBlockHash
-        }
-        this.newTransactions = [];
-        this.chain.push(newBlock)
-        return newBlock
+    constructor() {
+        this.chain = []; //contains all the blocks in a chain
+        this.pendingTransactions = []; //saves all transactions pending mining
     }
 
+    createNewBlock(nonce, previousBlockHash, hash) {
+        const newBlock = {
+            index: this.chain.length + 1, //appends an index to the new block
+            timeStamp: Date.now(), //puts a timestamp on the new block
+            transactions: this.pendingTransactions,
+            nonce,
+            hash,
+            previousBlockHash //hash of the previous block
+        };
+
+        this.pendingTransactions = []; //resets the new transaction array
+        this.chain.push(newBlock); //pushes the newly mined block into the chain
+        // return newBlock;
+    }
+
+    getLastBlock(){
+        return this.chain[this.chain.length - 1]
+    }
+
+    //creates a new transacton
+    createNewTransaction(amount, sender, recipient){
+        const newTransaction = {
+            amount,
+            sender,
+            recipient
+        }
+
+        this.pendingTransactions.push(newTransaction)
+
+        //gets the last block object in the chain and returns it
+        const newBlockNumber = this.getLastBlock()["index"]  + 1
+        return `Block Number = ${newBlockNumber}`
+    }
 }
+
+
 module.exports = Crunch
--- dev/test.js
+++ dev/test.js
@@ -1,8 +1,10 @@
 const Crunch = require("./crunch");
 
 const crunch = new Crunch();
-async function mainTest(){
+async function mainTest() {
     crunch.createNewBlock(72637, "82YRIUY274YR2", '726287463248H')
+    crunch.createNewBlock(72637, "8EUDUJD9J93DJ", 'NDI93U9D393HD')
+    crunch.createNewBlock(72637, "N3EUJ9UEE8D39", 'JS92UEJ83E3J3')
 
     console.log(crunch)
 }
